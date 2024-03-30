9ce514e7d319b4cb760d6dca5c8b2a84b071b128
EyeAtthasatsri <133437896+EyeAtthasatsri@users.noreply.github.com>
2024-03-29 23:08:00 -0400

    Add angular/material - ng add @angular/material

--- Tha_Atthasatsri_SuperBowl/src/main.ts
+++ Tha_Atthasatsri_SuperBowl/src/main.ts
@@ -7,18 +7,33 @@ import { importProvidersFrom } from '@angular/core';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService } from './app/services/in-memory-data.service';
 import { provideHttpClient } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 
-bootstrapApplication(AppComponent, 
-  // appConfig,
-  {
-    providers: [
-      provideHttpClient(),
-      provideProtractorTestingSupport(),
-        importProvidersFrom(HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {
-          dataEncapsulation: false })),
-    ]
-  }
-  )
+// bootstrapApplication(AppComponent, 
+//   // appConfig,
+//   {
+//     providers: [
+//       provideHttpClient(),
+//       provideProtractorTestingSupport(),
+//         importProvidersFrom(HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {
+//           dataEncapsulation: false }))provideAnimationsAsync(),
+//     ]
+//   }
+//   )
  
+// .catch((err) => console.error(err));
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideHttpClient(),
+    provideProtractorTestingSupport(),
+    importProvidersFrom(HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {
+      dataEncapsulation: false 
+    })),
+  ]
+})
+.then(() => {
+  provideAnimationsAsync(); // Call the function after bootstrapping the application
+})
 .catch((err) => console.error(err));
